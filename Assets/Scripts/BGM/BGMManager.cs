21c42d15c02c3a47a425cd6bea958de06dcb551d
Jhony Lucas <jhonyjl37@gmail.com>
2020-10-16 00:09:35 -0300

    Added bgm.

---
+++ Assets/Scripts/BGM/BGMManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{   
+    AudioSource _audio;
+    void Awake()
+    {
+      DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+}
